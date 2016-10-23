59ee9d6c7947726d6c6391060764ea59f40ee8f9
Chen Wang <gongfunaicha@gmail.com>
2016-10-22 19:13:45 -0600

    Command line interpreting finished. Reading the chainfile.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,98 @@
-#include <iostream>
+// Program awget
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+void commandline_error_message()
+{
+    cerr << "Error: Invalid parameter received." << endl;
+    cerr << "Usage: awget <URL> [-c chainfile]" << endl;
+    cerr << "If chainfile is not specified, program will look for chaingang.txt under the current directory." << endl;
+}
+
+void chainfile_error_message()
+{
+    cerr << "Error reading the chain file. Please check chain file format." << endl;
+}
+
+int parse_first_line(string line)
+{
+
+}
+
+int process_chain_file(string chainfilename, vector<string>& stepstones)
+{
+    ifstream chainfile;
+    chainfile.open(chainfilename);
+    string line;
+    if (!chainfile.is_open())
+    {
+        cerr << "Failed to open the chainfile. Does the file exist?" << endl;
+        return -1;
+    }
+
+    getline(chainfile, line);
+
+    if (chainfile)
+    {
+        chainfile_error_message();
+        return -1;
+    }
+
+    if (parse_first_line(line) == -1)
+    {
+        chainfile_error_message();
+        return -1;
+    }
+
+    while (!chainfile.eof())
+    {
+        getline(chainfile, line);
+        if (chainfile)
+            cout << line << endl;
+    }
+    chainfile.close();
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if ((argc == 2) || (argc == 4))
+    {
+        string url(argv[1]);
+
+        if ((argc == 4) && (strcmp("-c", argv[2]) != 0))
+        {
+            // Unexpected parameter at "-c"
+            commandline_error_message();
+            return -1;
+        }
+
+        string chainfile = "chaingang.txt";
+
+        if (argc == 4)
+        {
+            string temp(argv[3]);
+            chainfile = temp;
+        }
+
+        vector<string> stepstones;
+        stepstones.reserve(20);
+
+        if (!process_chain_file(chainfile, stepstones))
+            return -1;
+
+
+
+    }
+    else
+    {
+        // Wrong number of parameters
+        commandline_error_message();
+        return -1;
+    }
     return 0;
 }
